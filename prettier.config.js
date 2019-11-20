bda47ac457022afaae6d8216ed09fe1770609fe0
Janne Kurkinen <info@envision.fi>
2019-11-20 09:18:59 +0200

    Add Prettier config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 150,
+  tabWidth: 2,
+  tabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  tailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'ignore'
+};
